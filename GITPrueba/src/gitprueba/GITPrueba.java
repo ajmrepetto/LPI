6826c36d42b345d83a5a8c66d848ea39ac9f14a1
boincadm <boincadm@debian6.localdomain>
2014-10-15 10:45:56 -0400

    Esto es una pruebaen clase

---
+++ GITPrueba/src/gitprueba/GITPrueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitprueba;
+
+/**
+ *
+ * @author boincadm
+ */
+public class GITPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Prueba hecha en clase
+    }
+}
